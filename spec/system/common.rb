7a54c3e0941e05ce87bc4743d8d105238b390d9b
古川真宙 <maoki0523.22@gmail.com>
2021-04-13 11:31:30 +0900

    create rsepc test to top_page

---
+++ spec/system/common.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe '共通系', type: :system do
+  before do
+    visit root_path
+  end
+  describe 'トップページ' do
+    it 'トップページが正しく表示されている' do
+      expect(page).to have_content('使い方')
+      expect(page).to have_content('質問一覧')
+      expect(page).to have_content('新規登録')
+      expect(page).to have_content('ログイン')
+      expect(page).to have_content('プライバシ-ポリシー')
+    end
+  end
+end
