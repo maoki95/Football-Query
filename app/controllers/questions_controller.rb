f7b910eac3c5fc52cb5626b7a02e373556d914ed
古川真宙 <maoki0523.22@gmail.com>
2021-04-17 10:50:44 +0900

    add question post_fanction

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,4 +4,24 @@ class QuestionsController < ApplicationController
     @questions = Question.all.includes(:user)
   end
 
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = current_user.questions.build(question_params)
+    if @question.save
+      redirect_to questions_path, success: t('.success')
+    else
+      flash.now[:danger] = t('.fail')
+      render new_question_path
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body, :video, :picture, :youtube)
+  end
+
 end
