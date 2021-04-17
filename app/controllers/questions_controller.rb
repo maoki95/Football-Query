1d28eef85ecf03dfe4ac0277d84d394044a4098b
古川真宙 <maoki0523.22@gmail.com>
2021-04-17 11:15:48 +0900

    add question_post rsepec

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,5 @@
 class QuestionsController < ApplicationController
+  skip_before_action :require_login, only: %i[index]
 
   def index
     @questions = Question.all.includes(:user)
--- spec/system/question_spec.rb
+++ spec/system/question_spec.rb
@@ -24,4 +24,28 @@ RSpec.describe '質問', type: :system do
         end
     end
   end
+  context '入力情報正常系' do
+    it '質問が新規作成できること' do
+      login
+      visit '/questions/new'
+      expect {
+        fill_in 'Title', with: 'わかりません'
+        fill_in 'Body', with: '〇〇がわかりません'
+        click_button '質問する'
+      }.to change { Question.count }.by(1)
+      expect(page).to have_content('質問を投稿しました')
+    end
+  end
+
+  context '入力情報異常系' do
+    it '質問投稿できない' do
+      login
+      visit '/questions/new'
+      expect {
+        fill_in 'Body', with: '〇〇がわかりません'
+        click_button '質問する'
+      }.to change { Question.count }.by(0)
+      expect(page).to have_content('質問の投稿に失敗しました')
+    end
+  end
 end
