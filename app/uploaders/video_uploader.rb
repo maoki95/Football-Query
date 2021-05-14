0b99963437be9236cbe3d463a87a658f1bb0d34f
古川真宙 <maoki0523.22@gmail.com>
2021-05-14 21:36:38 +0900

    S3の設定

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -5,7 +5,11 @@ class VideoUploader < CarrierWave::Uploader::Base
   # process resize_to_fill: [100,100]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
